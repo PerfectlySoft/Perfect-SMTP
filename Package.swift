e2175e3fbf55852af5f6785a39803abd37b54b0a
Jonathan Guthrie <jono@guthrie.net.nz>
2017-01-05 12:24:24 -0500

    Update to use current 2.1 tag for http

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "PerfectSMTP",
     dependencies: [
-    .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2, minor: 0),
+    .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2, minor: 1),
     .Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 2, minor: 0)
   ]
 )
