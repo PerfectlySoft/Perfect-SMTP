1fb97ec726ee25b94874a6b049695ad8ce34117c
Rockford Wei <rocky@perfect.org>
2017-09-28 14:02:07 -0400

    Upgrading to Swift 4.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "PerfectSMTP",
     dependencies: [
-    .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2),
-    .Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 2)
+    .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 3),
+    .Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 3)
   ]
 )
