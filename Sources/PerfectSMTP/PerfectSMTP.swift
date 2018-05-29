876a8fa20fdbcd53adc4bf84048087d06232b3aa
Rockford Wei <rocky@perfect.org>
2018-05-29 17:09:40 -0400

    Formatting funcky indents.

--- Sources/PerfectSMTP/PerfectSMTP.swift
+++ Sources/PerfectSMTP/PerfectSMTP.swift
@@ -126,11 +126,11 @@ extension String {
 			guard let at = index(of: "@") else {
 				return self
 			}
-      #if swift(>=4.1)
-        return String(self[at..<endIndex])
-      #else
-        return self[at..<endIndex]
-      #endif
+			#if swift(>=4.1)
+				return String(self[at..<endIndex])
+			#else
+				return self[at..<endIndex]
+			#endif
 		}
 	}
 	
