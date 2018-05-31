65043d5be8422cc6719cb3f31a30535dd74b796f
Rockford Wei <rocky@perfect.org>
2018-05-31 14:50:28 -0400

    checkouts.

--- Package.swift
+++ Package.swift
@@ -3,13 +3,10 @@ import PackageDescription
 let package = Package(name: "PerfectSMTP", 
 	products: [.library(name: "PerfectSMTP",targets: ["PerfectSMTP"]),],
     dependencies: [
-		.package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", from: "3.0.6"),
+	.package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", from: "3.0.6"),
 	],
     targets: [
         .target(
             name: "PerfectSMTP",
             dependencies: ["PerfectCURL"]),
-        .testTarget(
-            name: "PerfectSMTPTests",
-            dependencies: ["PerfectSMTP"]),
     ])
