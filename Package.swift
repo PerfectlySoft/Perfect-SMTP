a9c307ce27a10475e35966f29449bfef60c43cc1
Rockford Wei <rocky@perfect.org>
2018-05-31 14:54:08 -0400

    adding tests

--- Package.swift
+++ Package.swift
@@ -9,4 +9,7 @@ let package = Package(name: "PerfectSMTP",
         .target(
             name: "PerfectSMTP",
             dependencies: ["PerfectCURL"]),
+        .testTarget(
+            name: "PerfectSMTPTests",
+            dependencies: ["PerfectSMTP"]),
     ])
