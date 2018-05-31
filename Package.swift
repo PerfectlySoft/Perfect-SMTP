24e5642e90ecee83e872031b3cd535779a007c56
Rockford Wei <rocky@perfect.org>
2018-05-31 14:47:54 -0400

    Fixing version conflicts.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(name: "PerfectSMTP", 
 	products: [.library(name: "PerfectSMTP",targets: ["PerfectSMTP"]),],
     dependencies: [
-		.package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", from: "3.0.6"),
 	],
     targets: [
         .target(
