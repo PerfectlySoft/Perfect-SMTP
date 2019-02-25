407fb83870181116ca060d8ea6f675f5aefc89a5
Kyle Jessup <kyle.jessup@treefrog.ca>
2019-02-25 12:04:48 -0500

    added base64 encoding for subject line - (emoji handling)

--- Sources/PerfectSMTP/PerfectSMTP.swift
+++ Sources/PerfectSMTP/PerfectSMTP.swift
@@ -324,7 +324,11 @@ public class EMail {
 		if subject.isEmpty {
 			throw SMTPError.INVALID_SUBJECT
 		} else {
-			body += "Subject: =?UTF-8?Q?\(subject)?=\r\n"
+			if let subjectB64 = subject.base64Encoded() {
+				body += "Subject: =?utf-8?B?\(subjectB64)?=\r\n"
+			} else {
+				body += "Subject: =?utf-8?Q?\(subject)?=\r\n"
+			}
 		}
 		// mark the content type
 		body += "MIME-Version: 1.0\r\nContent-type: multipart/alternative; boundary=\"\(boundary)\"\r\n\r\n"
