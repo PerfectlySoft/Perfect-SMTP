a051f336d1c63b37ba1eea5685db1aa57a17224d
Kyle Jessup <kyle.jessup@treefrog.ca>
2020-07-14 18:07:33 -0400

    version bumps

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.1
 import PackageDescription
-let package = Package(name: "PerfectSMTP", 
+let package = Package(name: "PerfectSMTP",
+	platforms: [
+		.macOS(.v10_15)
+	],
 	products: [.library(name: "PerfectSMTP",targets: ["PerfectSMTP"]),],
     dependencies: [
-		.package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", from: "4.0.0"),
-		.package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", from: "5.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", from: "4.0.0"),
 		.package(url: "https://github.com/PerfectlySoft/Perfect-MIME.git", from: "1.0.0"),
 	],
     targets: [
